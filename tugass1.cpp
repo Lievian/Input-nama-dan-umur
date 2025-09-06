63d0bac244b4280e090c2a05bbe2bfb9ddfeb28f
Lievian <rizalherudin8@gmail.com>
2025-09-06 21:44:27 +0700

    Add files via upload

---
+++ tugass1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+int main () 
+{
+    string nama ;
+    int umur ;
+
+    cout << "Halo, selamat datang di Fasilkom Informatika Unsika!" << endl ;
+    cout << "Siapa nama kamu : " ;
+    cin >> nama ;
+    cout << "Berapa umur kamu : " ;
+    cin >> umur ;
+    cout << "Halo " << nama << ", umur kamu " << umur << "." << endl ;
+    return 0 ;
+}
